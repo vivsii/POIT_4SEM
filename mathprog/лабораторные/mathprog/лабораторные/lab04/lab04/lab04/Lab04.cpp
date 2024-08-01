5405fa7cb4930df944f65de7a03cc147479491dd
vivsii <116980278+vivsii@users.noreply.github.com>
2024-08-01 22:56:23 +0300

    Add files via upload

---
+++ mathprog/лабораторные/mathprog/лабораторные/lab04/lab04/lab04/Lab04.cpp
@@ -0,0 +1,125 @@
+#include "stdafx.h"
+#include <cmath>
+#include <memory.h>
+#include <algorithm>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include "Levenstein.h"
+#include "MultyMatrix.h"
+using namespace std;
+
+#define N 6
+#define FIRST_LEN 120
+#define SECOND_LEN 110
+
+char* Task1(int size)
+{
+	char* str = new char[size];
+
+	for (int i = 0; i < size; i++) str[i] = rand() % 26 + 'a';
+	return str;
+}
+
+void Task5()
+{
+	srand(time(NULL));
+	clock_t t1 = 0, t2 = 0, t3 = 0, t4 = 0;
+	int Mc[N + 1] = { 7, 10, 18, 21, 28, 38, 49 }, Ms[N][N], r = 0, rd = 0;
+	memset(Ms, 0, sizeof(int) * N * N);
+	t1 = clock();
+	r = OptimalM(1, N, N, Mc, OPTIMALM_PARM(Ms));
+	t2 = clock();
+
+	std::cout << std::endl << "-- расстановка скобок (рекурсивное решение) " << std::endl;
+	std::cout << std::endl << "затраченное время (сек):  " << ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
+	std::cout << std::endl << "размерности матриц: ";
+	for (int i = 1; i <= N; i++)
+		std::cout << "(" << Mc[i - 1] << "," << Mc[i] << ") ";
+	std::cout << std::endl << "минимальное количество операций умножения: " << r;
+	std::cout << std::endl << "\nматрицаS";
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << std::endl;
+		for (int j = 0; j < N; j++)  std::cout << Ms[i][j] << "  ";
+	}
+	std::cout << std::endl;
+
+	memset(Ms, 0, sizeof(int) * N * N);
+	t3 = clock();
+	rd = OptimalMD(N, Mc, OPTIMALM_PARM(Ms));
+	t4 = clock();
+	std::cout << std::endl
+		<< "-- расстановка скобок (динамическое программирование) " << std::endl;
+	std::cout << std::endl << "затраченное время (сек):  " << ((double)(t4 - t3)) / ((double)CLOCKS_PER_SEC);
+	std::cout << std::endl << "размерности матриц: ";
+	for (int i = 1; i <= N; i++)
+		std::cout << "(" << Mc[i - 1] << "," << Mc[i] << ") ";
+	std::cout << std::endl << "минимальное количество операций умножения: " << rd;
+	std::cout << std::endl << "\nматрицаS";
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << std::endl;
+		for (int j = 0; j < N; j++)  std::cout << Ms[i][j] << "  ";
+	}
+	std::cout << std::endl << std::endl;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+
+	char* s1 = Task1(FIRST_LEN);
+	cout << "S1: " << endl;
+	for (int i = 0; i < FIRST_LEN; i++)
+	{
+		if (i % 50 == 0)
+		{
+			cout << "\n";
+		}
+		cout << s1[i];
+	}
+	cout << endl << endl;
+
+	srand(time(NULL) + 1);
+	char* s2 = Task1(SECOND_LEN);
+	cout << "S2: " << endl;
+	for (int i = 0; i < SECOND_LEN; i++)
+	{
+		if (i % 50 == 0)
+		{
+			cout << "\n";
+		}
+		cout << s2[i];
+	}
+	cout << endl << endl;
+
+	clock_t t1 = 0, t2 = 0, t3 = 0, t4 = 0;
+	int lx = sizeof(s1);
+	int ly = sizeof(s2);
+
+	int s1_size[]{ FIRST_LEN / 25, FIRST_LEN / 20, FIRST_LEN / 15, FIRST_LEN / 10, FIRST_LEN / 5, FIRST_LEN / 2, FIRST_LEN };
+	int s2_size[]{ SECOND_LEN / 25, SECOND_LEN / 20, SECOND_LEN / 15, SECOND_LEN / 10, SECOND_LEN / 5, SECOND_LEN / 2, SECOND_LEN };
+
+	cout << "\n\n-- расстояние Левенштейна -----";
+	cout << "\n\n-- длина --- рекурсия -- дин.програм. ---\n";
+
+	for (int i = 0; i < min(lx, ly); i++)
+	{
+		t1 = clock();
+		levenshtein_r(s1_size[i], s1, s2_size[i], s2);
+		t2 = clock();
+
+		t3 = clock();
+		levenshtein(s1_size[i], s1, s2_size[i], s2);
+		t4 = clock();
+		cout << right << setw(2) << s1_size[i] << "/" << setw(2) << s2_size[i]
+			<< "        " << left << setw(10) << (t2 - t1)
+			<< "   " << setw(10) << (t4 - t3) << endl;
+	}
+
+	Task5();
+	system("pause");
+	return 0;
+}
